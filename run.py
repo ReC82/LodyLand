1da86c0c4ec77934eab1d26855cd3432db56c2ec
ReC82 <lloyd.malfliet@gmail.com>
2025-11-25 14:22:14 +0100

    add run.py

--- run.py
+++ run.py
@@ -3,7 +3,10 @@
 # Purpose: Entry point for development. Starts the minimal Flask app.
 # =============================================================================
 # run.py
+from app.data.normalize_all_yaml import run_all_normalizers
 from app import create_app
+
+run_all_normalizers()
 
 app = create_app()
 
